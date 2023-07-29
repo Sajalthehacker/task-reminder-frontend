75a610476b45ca43bd6d81b364021f7291e3d8e4
sajal mangal <sajalmangal220602@gmail.com>
2023-07-29 20:07:29 +0530

    fixed issues

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,27 +1,52 @@
-import React, { useState } from 'react'
-import './Home.css'
-import DateTimePicker from 'react-datetime-picker'
+import React, { useEffect, useState } from 'react';
+import './Home.css';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'; // Import the styles
+
 
 const Home = () => {
-    const [reminderMsg, setReminderMsg] = useState("")
-    const [remindAt, setRemindAt] = useState()
-    const [reminderList, setReminderList] = useState([])
+    const [reminderMsg, setReminderMsg] = useState("");
+    const [remindAt, setRemindAt] = useState(new Date());
+    const [reminderList, setReminderList] = useState([]);
 
     useEffect(() => {
-        axios.get("http://localhost:5000/api/reminder/getAllReminders").then(res => setReminderList(res.data.message))
-    }, [])
+        const fetchData = async () => {
+            const { data } = await axios.get("http://localhost:5000/api/reminder/getAllReminders")
 
-    const addReminder = () => {
-        axios.post("http://localhost:5000/api/reminder/addReminders", { reminderMsg, remindAt })
-            .then(res => setReminderList(res.data.message))
-        setReminderMsg("")
-        setRemindAt()
-    }
+            if (data.status === "SUCCESSFULLY_FETCHED") {
+                setReminderList(data.message)
+            }
+            else {
+                alert(data.message)
+            }
+        }
+        fetchData()
+    }, []);
 
-    const deleteReminder = (id) => {
-        axios.post("http://localhost:5000/api/reminder//deleteReminders", { id })
-            .then(res => setReminderList(res.data.message))
-    }
+    const addReminder = async () => {
+        const { data } = await axios.post("http://localhost:5000/api/reminder/addReminders", { reminderMsg, remindAt })
+
+        if (data.status === "SUCCESSFULLY_FETCHED") {
+            setReminderList(data.message)
+        }
+        else {
+            alert(data.message)
+        }
+        setReminderMsg("");
+        setRemindAt(new Date());
+    };
+
+    const deleteReminder = async (id) => {
+        const { data } = await axios.post("http://localhost:5000/api/reminder/deleteReminders", { id })
+
+        if (data.status === "SUCCESSFULLY_FETCHED") {
+            setReminderList(data.message)
+        }
+        else {
+            alert(data.message)
+        }
+    };
 
     return (
         <div className='homepage-container'>
@@ -29,34 +54,33 @@ const Home = () => {
                 <div className="homepage_header">
                     <h1>Remind Me 🙋‍♂️</h1>
                     <input type="text" placeholder="Reminder notes here..." value={reminderMsg} onChange={e => setReminderMsg(e.target.value)} />
-                    <DateTimePicker
-                        value={remindAt}
-                        onChange={setRemindAt}
+                    <DatePicker
+                        selected={remindAt}
+                        onChange={date => setRemindAt(date)}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={1}
+                        dateFormat="MMMM d, yyyy h:mm aa"
                         minDate={new Date()}
-                        minutePlaceholder="mm"
-                        hourPlaceholder="hh"
-                        dayPlaceholder="DD"
-                        monthPlaceholder="MM"
-                        yearPlaceholder="YYYY"
                     />
+
+
                     <div className="button" onClick={addReminder}>Add Reminder</div>
                 </div>
 
                 <div className="homepage_body">
-                    {
-                        reminderList.map(reminder => (
-                            <div className="reminder_card" key={reminder._id}>
-                                <h2>{reminder.reminderMsg}</h2>
-                                <h3>Remind Me at:</h3>
-                                <p>{String(new Date(reminder.remindAt.toLocaleString(undefined, { timezone: "Asia/Kolkata" })))}</p>
-                                <div className="button" onClick={() => deleteReminder(reminder._id)}>Delete Reminder</div>
-                            </div>
-                        ))
-                    }
+                    {reminderList.length === 0 ? null : reminderList.map(reminder => (
+                        <div className="reminder_card" key={reminder._id}>
+                            <h2>{reminder.reminderMessage}</h2>
+                            <h3>Remind Me at:</h3>
+                            <p>{String(new Date(reminder.remindAt.toLocaleString(undefined, { timezone: "Asia/Kolkata" })))}</p>
+                            <div className="button" onClick={() => deleteReminder(reminder._id)}>Delete Reminder</div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
